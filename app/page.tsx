b841083a029d6ded48cd04d7944abc3f9bcaacda
New Name <abz.siddique@gmail.com>
2025-05-07 12:27:13 +0100

    style: adjust for style guide values

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ const notoSans = Noto_Sans({
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-gray-50 to-gray-300 h-screen flex items-start justify-center w-full min-w-[375px]">
+    <div className="bg-gradient-to-br from-[#f9fafb] to-[#d2d6db] h-screen flex items-start justify-center w-full min-w-[375px]">
     <div className={`${notoSans.className} flex flex-col mt-[200px] bg-white rounded-lg p-6 w-[340px]`}>
       <div className="flex gap-4 items-center mb-4">
         <Image src="/profile-thumbnail.png" alt="profile" width={48} height={48} />
         <div className="flex flex-col">
-          <div className="text-gray-900 text-lg font-semibold">Sarah Dole</div>
-          <div className="text-gray-600 text-sm font-normal">@sarahdole</div>
+          <div className="text-neutral-900 text-lg font-semibold">Sarah Dole</div>
+          <div className="text-neutral-600 text-sm font-normal">@sarahdole</div>
         </div>
       </div>
       <p className="font-normal text-base text-gray-600">
